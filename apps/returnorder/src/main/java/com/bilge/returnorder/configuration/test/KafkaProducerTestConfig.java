1b30a52117f850cbf694f441ab78bc87609f210f
bilgehan.aygun <bilgehan.aygun@tubitak.gov.tr>
2024-10-07 22:00:21 +0300

    KafkaProducerTestConfig icerisindeki gereksiz kafka template configurasyonu kaldirildi

--- apps/returnorder/src/main/java/com/bilge/returnorder/configuration/test/KafkaProducerTestConfig.java
+++ apps/returnorder/src/main/java/com/bilge/returnorder/configuration/test/KafkaProducerTestConfig.java
@@ -1,17 +1,10 @@
 package com.bilge.returnorder.configuration.test;
 
 import com.bilge.returnorder.configuration.kafka.BaseProducer;
-import org.apache.kafka.clients.producer.ProducerConfig;
-import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
-import org.springframework.kafka.core.DefaultKafkaProducerFactory;
-import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.kafka.core.ProducerFactory;
-import org.springframework.kafka.support.serializer.JsonSerializer;
 
-import java.util.HashMap;
 import java.util.Map;
 
 @TestConfiguration
@@ -29,20 +22,6 @@ public class KafkaProducerTestConfig {
 
     return producerBeans.values().stream()
       .map(BaseProducer::getTopic).distinct().toArray(String[]::new);
-  }
-
-  @Bean
-  public <T> ProducerFactory<String, T> producerFactory() {
-    Map<String, Object> configProps = new HashMap<>();
-    configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, System.getProperty("spring.kafka.bootstrap-servers"));
-    configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-    configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
-    return new DefaultKafkaProducerFactory<>(configProps);
-  }
-
-  @Bean
-  public <T> KafkaTemplate<String, T> kafkaTemplate(ProducerFactory<String, T> producerFactory) {
-    return new KafkaTemplate<>(producerFactory);
   }
 
   @Bean
